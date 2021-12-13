86faae0edbe75ef7b3aa69d5826c833a76ed6b26
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-13 20:39:20 +0530

    used get request to pass name as a parameter

--- src/main/java/com/bridgelabz/springrestapicalls/controller/HelloWordController.java
+++ src/main/java/com/bridgelabz/springrestapicalls/controller/HelloWordController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.springrestapicalls.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,9 @@ public class HelloWordController {
 	public String sayHello() {
 		return "Hello from bridgelabz!";
 	}
+	
+	@RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
 }
