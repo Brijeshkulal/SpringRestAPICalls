b4eca42da8262f666f32b118c8f99819c93c3323
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-13 20:12:46 +0530

    welcome message

---
+++ src/main/java/com/bridgelabz/springrestapicalls/SpringRestApiCallsApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrestapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiCallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiCallsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bridgelabz/springrestapicalls/controller/HelloWordController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springrestapicalls.controller;
+
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/hello")
+//public class HelloWordController {
+//	@RequestMapping(value = { "", "/", "/home" })
+//	public String sayHello() {
+//		return "Hello from bridgelabz!";
+//	}
+//}
