8f1f8d35a723a97630e6b4c243a9d2b5d90c02e2
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-13 21:58:13 +0530

    Added post request method

--- src/main/java/com/bridgelabz/springrestapicalls/controller/HelloWordController.java
+++ src/main/java/com/bridgelabz/springrestapicalls/controller/HelloWordController.java
@@ -2,10 +2,14 @@ package com.bridgelabz.springrestapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.springrestapicalls.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -21,7 +25,12 @@ public class HelloWordController {
     }
 	
 	 @GetMapping("/param/{name}")
-	    public String sayHelloParam(@PathVariable String name) {
-	        return "Hello " + name + "!";
+	 public String sayHelloParam(@PathVariable String name) {
+		 return "Hello " + name + "!";
+	    }
+	 
+	 @PostMapping("/post")
+	 public String sayHello(@RequestBody User user) {
+		 return "Hello " + user.getFirstName() + " " + user.getLastName();
 	    }
 }
